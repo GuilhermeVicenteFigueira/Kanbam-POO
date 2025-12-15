83f3e92fb57a492063c4eade87921aa6e941bc36
GuilhermeVicenteFigueira <figueira.gui1@gmail.com>
2025-12-14 21:57:31 -0300

    Feature finalização do projeto

--- src/Main.java
+++ src/Main.java
@@ -5,12 +5,12 @@ import Enums.Status;
 import Service.TeskManeger;
 
 void main() {
-    User user1 = new User("Guilherme", "figueira.gui1@gmail.com","Casadogui123!@",1);
+    User user1 = new User("Guilherme", "figueira.gui1@gmail.com","ABCD123!@",1);
     TeskManeger teskManeger = new TeskManeger();
 
-    teskManeger.createTesk(1,"ëstudar java","Estudar java pelo devDojo ou projeto solo",user1, Status.GETTING_STARTED, Priority.LOW);
+    teskManeger.createTesk(1,"Estudar java","Estudar java pelo devDojo ou projeto solo",user1, Status.GETTING_STARTED, Priority.LOW);
     teskManeger.listTesks();
-    teskManeger.editTesk(1,"chupar pintios", "Ser o diero", Priority.CRITICAL, Status.PENDING, user1);
+    teskManeger.editTesk(1,"Estudar Angular", "", Priority.CRITICAL, Status.PENDING, user1);
     teskManeger.listTesks();
     teskManeger.nextStage(1);
     teskManeger.listTesks();
